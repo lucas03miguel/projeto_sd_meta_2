651b04b301120f996af40a0b1760418f7319ae13
Lucas <lucas.m.loberto@gmail.com>
2024-05-16 17:12:19 +0100

    TEMOS O LOGINNNNNNNNNN

--- src/main/java/com/example/projetoSD/ProjetoSDApplication.java
+++ src/main/java/com/example/projetoSD/ProjetoSDApplication.java
@@ -1,11 +1,10 @@
-package com.example.projetoSD.web;
+package com.example.projetoSD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoSDApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSDApplication.class, args);
 	}
