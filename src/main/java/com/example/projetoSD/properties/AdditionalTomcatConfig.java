424c3febdd2a480d734d0fa7ba81e2858b2b673a
Lucas <lucas.m.loberto@gmail.com>
2024-05-17 01:46:54 +0100

    ja da

---
+++ src/main/java/com/example/projetoSD/properties/AdditionalTomcatConfig.java
@@ -0,0 +1,22 @@
+package com.example.projetoSD.properties;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.catalina.connector.Connector;
+
+@Configuration
+public class AdditionalTomcatConfig {
+    
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.addAdditionalTomcatConnectors(createStandardConnector());
+    }
+    
+    private Connector createStandardConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(8080);
+        return connector;
+    }
+}
